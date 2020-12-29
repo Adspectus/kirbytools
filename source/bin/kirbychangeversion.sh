2a1bef42b6da43c0e7e541d51efb1a4951290853
Uwe Gehring <uwe@imap.cc>
2020-12-29 10:50:24 +0100

    First working version

--- source/bin/kirbychangeversion.sh
+++ source/bin/kirbychangeversion.sh
@@ -18,13 +18,13 @@ usage() {
   echo ""
   echo "  Usage:"
   echo ""
-  echo "  $(basename $0) [-h] [-d] [-l] [-f] [-v <current|versionnumber>] -w <vhost>"
+  echo "  $(basename $0) [-h] [-d] [-t] [-v <versionnumber> | -r] -w <vhost>"
   echo ""
   echo "  -h: Show this help"
   echo "  -d: Show debug information"
-  echo "  -l: Replace the kirby program directory with a symbolic link to $KIRBYLIBDIR/[versionnumber]"
-  echo "  -f: Force download even if package is already in $KIRBYDOWNLOADDIR"
-  echo "  -v: version to be installed (default: ${txtbld}$KIRBYVERSION${txtrst})"
+  echo "  -t: Test only"
+  echo "  -v: version to be installed (default: highest patch version for installed minor version)"
+  echo "  -r: revert to previous version"
   echo "  -w: virtual hostname(s) for which the version should be changed (wildcards ok)"
   echo ""
   exit 1
@@ -32,14 +32,13 @@ usage() {
 
 ## Get commandline options
 debMsg "Get commandline options"
-while getopts ":hdlfp:v:w:" opt;do
+while getopts ":hdtrv:w:" opt;do
   case "${opt}" in
     h)  usage;;
     d)  DEBUG=1;;
-    l)  LINKKIRBY=1;;
-    f)  FORCEDOWNLOAD=1;;
-    p)  package=$OPTARG;;
+    t)  TEST=1;;
     v)  version=$OPTARG;;
+    r)  REVERT=1;;
     w)  opt_w=1;vhost=$OPTARG;;
     :)  errMsg "-$OPTARG requires an argument" && usage;;
     *)  errMsg "Invalid option $OPTARG" && usage;;
@@ -49,83 +48,125 @@ shift $((OPTIND -1))
 
 [[ $opt_w ]] || { errMsg "-w is required" && usage; }
 
-## Check if version has valid value
-debMsg "Check if version has valid value"
-version=${version:-$KIRBYVERSION}
-if ! isValidKirbyVersion $version $KIRBYKIT;then
-  errMsg "Invalid version $version" && usage
-fi
-
-## Download the required version if not already and extract kirby program dir
-kirbydownload -l -v $version > /dev/null
-[[ $? -ne 0 ]] && errMsg "Could not download $version" && usage
-
 ## Print info in debug mode
 [[ $DEBUG ]] && showVars
 
+## Check if version is set and has valid value
+if [[ -n "$version" ]];then
+  debMsg "Check if version has valid value"
+  if ! isValidKirbyVersion $version $KIRBYKIT;then
+    errMsg "Invalid version $version" && usage
+  fi
+fi
+
+[[ $TEST ]] && echo -e "\n${txtblue}Testing only!${txtrst}\n"
+
+## Loop through all vhosts and check if it is a Kirby vhost at all
+## If it is Kirby, determine the version by means of composer.json
 for vh in $(find $KIRBYVHOSTROOT -maxdepth 1 -type d -name ${vhost[*]});do
-  debMsg "Found virtual host in $vh"
-  kirbydir=$(find $vh \( -type d -or -type l \) -name kirby)
-  if [[ -n "$kirbydir" ]];then
-    debMsg "Found 'kirby' directory $kirbydir"
-    thisversion=$(jq -r '.version' $kirbydir/composer.json)
-    debMsg "Current Kirby version: $thisversion"
+  thisvhost=$(basename $vh)
+  debMsg "Found virtual host $thisvhost in $(dirname $vh)"
+  kirbyvhost=$(find $vh -type f -name .kirbydocroot)
+  if [ -n "$kirbyvhost" ];then
+    docroot=$(dirname $kirbyvhost)
+    $PHPBIN -f /usr/share/kirbytools/getConfig.php $docroot | jq '.' > $KIRBYTEMPDIR/$(basename $vh).json
+    thisversion=$(jq -r '.version' $KIRBYTEMPDIR/$(basename $vh).json)
+    if [ -z $thisversion ];then
+      errMsg "Could not determine this Kirby version!" && continue
+    fi
+    echo "Kirby virtual host $thisvhost is version $thisversion"
+
 # If the target version has not been set on command line, we will only upgrade
 # to the latest patch version of the current version, so we will determine it
-    if [ "$version" == "current" ];then
+    if [[ -z "$version" ]];then
+      if [[ $REVERT ]];then
+        debMsg "Reverting to previous Kirby version"
+        if [ -f $docroot/.kirbypreviousversion ];then
+          targetversion=$(cat $docroot/.kirbypreviousversion)
+          debMsg "Previous Kirby version: $targetversion"
+        else
+          errMsg "-r is only valid if there was a previous kirby version from which this program has made an up/downgrade" && continue
+        fi
+      else
+        thisversionmajor=$(echo $thisversion | cut -d"." -f 1)
+        thisversionminor=$(echo $thisversion | cut -d"." -f 2)
+  #      currentversion=$(getHighestPatchVersion $KIRBYKIT $(getHighestMinorVersion $KIRBYKIT $thisversionmajor))
+        targetversion=$(getHighestPatchVersion $KIRBYKIT $thisversionmajor.$thisversionminor)
+        debMsg "Last patch for this version: $targetversion"
+  #      debMsg "Current available version: $currentversion"
+      fi
     else
-      thisversionmajor=$(echo $thisversion | cut -d"." -f 1)
-      thisversionminor=$(echo $thisversion | cut -d"." -f 2)
-      lastversion=$(getHighestPatchVersion $KIRBYKIT $(getHighestMinorVersion $KIRBYKIT $thisversionmajor))
-      lastpatchversion=$(getHighestPatchVersion $KIRBYKIT $thisversionmajor.$thisversionminor)
-      debMsg "Last patch for this version: $lastpatchversion"
-      debMsg "Last available version: $lastversion"
+      targetversion="$version"
     fi
-    if [ -f $(dirname $kirbydir)/.previouskirbyversion ];then
-      previousversion=$(cat $(dirname $kirbydir)/.previouskirbyversion)
-      debMsg "Previous Kirby version: $previousversion"
+
+    if [ "$thisversion" == "$targetversion" ];then
+      echo "${txtbold}This Kirby version is already the required version ($targetversion)!${txtrst}"
+      continue
     fi
-    kirbypath=$(realpath $kirbydir)
-    debMsg "Real 'kirby' directory: $kirbypath"
-    if [ "$kirbydir" == "$kirbypath" ];then
-      debMsg "$kirbydir is a real directory"
-    else
-      debMsg "$kirbydir is a symbolic link pointing to $kirbypath"
+
+    if [ ! $TEST ];then
+      kirbydownload -l -v $targetversion > /dev/null
+      [[ $? -ne 0 ]] && errMsg "Could not download $targetversion" && usage
+    fi
+
+    [[ "$thisversion" < "$targetversion" ]] && action="Upgrade"
+    [[ "$thisversion" > "$targetversion" ]] && action="Downgrade"
+    echo "This Kirby version will be ${action,,}d to version $targetversion"
+
+    kirbydir=$(jq -r '.roots.kirby' $KIRBYTEMPDIR/$(basename $vh).json)
+    if [ "$kirbydir" == "$docroot/kirby" ];then # Case 1: kirby is a directory in docroot (standard)
+      debMsg "kirby is real directory $kirbydir"
+      debMsg "Deleting old directory $kirbydir"
+      [[ $TEST ]] && echo "${txtblue}Testing: kirby directory would be deleted${txtrst}"
+      [[ $TEST ]] || save_rm "$kirbydir"
+      if [ $? ];then
+        debMsg "Creating kirby directory and copying $KIRBYLIBDIR/$targetversion"
+        [[ $TEST ]] && echo "${txtblue}Testing: kirby directory would be created and $KIRBYLIBDIR/$targetversion/. would be copied into it${txtrst}"
+        [[ $TEST ]] || { save_mkdir "$kirbydir" && save_cp "$KIRBYLIBDIR/$targetversion/." "$kirbydir"; }
+        [[ $? ]] && SUCCESS=1
+      else
+        errMsg "Link/Directory $kirbydir could not be deleted, nothing is changed"
+      fi
+    elif [ "$kirbydir" == "$(readlink -f $docroot/kirby)" ];then # Case 2: kirby is a symlink to another dir
+      debMsg "kirby is a symlink to $kirbydir"
+      debMsg "Deleting old link $kirbydir"
+      [[ $TEST ]] && echo "${txtblue}Testing: kirby link would be deleted${txtrst}"
+      [[ $TEST ]] || save_rm "$kirbydir"
+      if [ $? ];then
+        debMsg "Creating kirby link and pointing to $KIRBYLIBDIR/$targetversion"
+        [[ $TEST ]] && echo "${txtblue}Testing: kirby link with target $KIRBYLIBDIR/$targetversion would be created${txtrst}"
+        [[ $TEST ]] || save_ln "$KIRBYLIBDIR/$targetversion" "$kirbydir"
+        [[ $? ]] && SUCCESS=1
+      else
+        errMsg "Link/Directory $kirbydir could not be deleted, nothing is changed"
+      fi
+      if [ ! $SUCCESS ];then
+        errMsg "$action not successful, restoring previous version"
+      else
+        [[ $TEST ]] || echo "${txtgreen}Kirby virtual host $thisvhost ${action,,}d from version $thisversion to version $targetversion${txtrst}"
+        [[ $TEST ]] || echo "$thisversion" > $docroot/.kirbypreviousversion
+        mediadir=$(jq -r '.roots.media' $KIRBYTEMPDIR/$(basename $vh).json)
+        if [ -d "$mediadir" ];then
+          debMsg "Found 'media' directory $mediadir"
+          for md in $(find "$mediadir" -mindepth 1 -maxdepth 1 -type d);do
+            debMsg "Deleting directory $md"
+            [[ $TEST ]] && echo "${txtblue}Testing: Directory $md would be deleted${txtrst}"
+            [[ $TEST ]] || save_rm "$md"
+            if [ $? ];then
+              [[ $TEST ]] || echo "${txtgreen}Directory $md deleted${txtrst}"
+            else
+              [[ $TEST ]] || errMsg "Could not delete directory $md"
+            fi
+          done
+        fi
+      fi
+    else # Case 3: kirby is anything else, but this will not be considered
+      debMsg "kirby is neither a real directory nor a symlink, skipping" && continue
     fi
   else
     errMsg "$vh is not a Kirby vhost"
   fi
 done
-
-# version=$(jq -r '.version' composer.json)
-exit
-
-## Check first if package is already downloaded. If not, download package
-if [[ $FORCEDOWNLOAD || ! -f $KIRBYDOWNLOADDIR/$package.tar.gz ]];then
-  [[ $LINKKIRBY ]] && kirbydownload -f -l -k $kit -v $ver > /dev/null
-  [[ $LINKKIRBY ]] || kirbydownload -f -k $kit -v $ver > /dev/null
-  [[ $? -ne 0 ]] && errMsg "Could not download $package" && usage
-fi
-
-save_mkdir "$KIRBYVHOSTDIR"
-tar -xzf $KIRBYDOWNLOADDIR/$package.tar.gz -C "$KIRBYTEMPDIR"
-save_cp "$KIRBYTEMPDIR/$package/." "$KIRBYVHOSTDIR"
-
-echo -e "\n${txtgreen}Kirby $package installed to $KIRBYVHOSTDIR${txtrst}\n"
-
-if [ $LINKKIRBY ];then
-  if [ ! -d $KIRBYLIBDIR/$ver ];then
-    save_mkdir "$KIRBYLIBDIR/$ver"
-    save_cp "$KIRBYVHOSTDIR/kirby/." "$KIRBYLIBDIR/$ver"
-  fi
-  save_rm "$KIRBYVHOSTDIR/kirby"
-  save_ln "$KIRBYLIBDIR/$ver" "$KIRBYVHOSTDIR/kirby"
-  if [ -w "$KIRBYVHOSTDIR/index.php" ];then
-    sed -i.bak -e "s/Kirby)/Kirby\(\[ 'roots' => \[ 'index' => __DIR__ \] \]\)\)/" $KIRBYVHOSTDIR/index.php
-  else
-    sudo sed -i.bak -e "s/Kirby)/Kirby\(\[ 'roots' => \[ 'index' => __DIR__ \] \]\)\)/" $KIRBYVHOSTDIR/index.php
-  fi
-fi
 
 debMsg "Script $(basename $0) finished successfully"
 
